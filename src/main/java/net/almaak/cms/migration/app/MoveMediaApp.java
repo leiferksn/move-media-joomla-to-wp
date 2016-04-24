4ac78bbc0157aacbfba537461b2e79e0f8c1c372
leif erikson <leiferksn@als-macbook.fritz.box>
2016-04-24 23:36:55 +0300

    Initial commit

---
+++ src/main/java/net/almaak/cms/migration/app/MoveMediaApp.java
@@ -0,0 +1,16 @@
+package net.almaak.cms.migration.app;
+
+/**
+ * Entry point class
+ *
+ * @author leiferksn
+ * @version %I%, %G%
+ */
+
+public class MoveMediaApp {
+
+    public static void main(String[] args){
+
+    }
+
+}
